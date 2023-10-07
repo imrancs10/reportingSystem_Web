83975ad18466d3a9134826e59b302ebf32590c7d
RAGHAV GARG <raghavgarg.hcst.cs20@sgei.org>
2023-10-07 22:14:11 +0530

    change-password + bugs fixed

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   {
     path: 'change-password',
     component: ForgetPasswordComponent,
-    // canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: '',
