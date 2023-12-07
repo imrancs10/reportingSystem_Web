130fc24589f74b1e70ecce6b98754cd15108d4f0
RAGHAV GARG <raghavgarg.hcst.cs20@sgei.org>
2023-12-07 08:16:28 +0530

    http -> https

--- src/app/services/setup.ts
+++ src/app/services/setup.ts
@@ -1,5 +1,5 @@
 export const setup = {
     production: true,
     API_URL_LOCAL: 'https://localhost:7216/v1/',
-    API_URL: 'http://api.imgdotpix.in/v1/',
+    API_URL: 'https://api.imgdotpix.in/v1/',
 }
